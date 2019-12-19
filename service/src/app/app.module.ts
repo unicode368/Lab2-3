21ef0b626e2f17936242eb5eff1c4c304c9b948f
lamecode <https://github.com/DreamTeamAccomplisher>
2019-12-19 11:54:49 +0200

    implementing @angular/common

--- service/src/app/app.module.ts
+++ service/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPGComponent } from './main-pg/main-pg.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
