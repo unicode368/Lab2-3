a20d371e85e2e878154b345c844975026b95e6bb
lamecode <kislaya.sliva@gmail.com>
2019-12-21 23:43:44 +0200

    LOGIN SERVICE

--- service/src/app/login/login.component.ts
+++ service/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ export class LoginComponent implements OnInit {
 
   loginUserData = {};
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
   }
